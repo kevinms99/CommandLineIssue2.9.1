0bc4aab345422a65bc05f813bfd19a4af0970990
kevinms99 <kevinms99@gmail.com>
2022-05-19 16:46:50 -0400

    Initial commit

---
+++ CommandLineIssue/CommandLineIssue/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandLineIssue;
+
+using CommandLine;
+
+public class Program
+{
+    public static void Main()
+    {
+        var optionTypes = from type in Assembly.GetExecutingAssembly().GetTypes()
+                          where type.GetCustomAttributes<OptionAttribute>().Cast<OptionAttribute>().Any()
+                          select type;
+    }
+}
